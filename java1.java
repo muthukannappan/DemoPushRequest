71ae75d8489ead39ceaa40a66f131ba1fb7553ad
muthukannappan <21cs039@acetcbe.edu.in>
2026-03-04 16:57:34 +0530

    New Code added

---
+++ java1.java
@@ -0,0 +1,17 @@
+// Package missing 
+
+class crud
+{
+    int dlt[]=new int[4];
+   
+    //performadd 
+    //perform delete operation
+}
+//Unused Method
+public static void performadd(int add){
+    
+     
+}
+//UnusedMethod
+public static void perfromdlt(int dlt) {
+}
